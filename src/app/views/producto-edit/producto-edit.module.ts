901418817d498cf9bc44f3f422fc53f35128fc23
Johann Felipe Rojas <jorojasp1@gmail.com>
2019-10-27 13:23:12 -0500

    With Rate - Before btn Edit

--- src/app/views/producto-edit/producto-edit.module.ts
+++ src/app/views/producto-edit/producto-edit.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { ProductoEditPage } from './producto-edit.page';
+import { IonicRatingModule } from 'ionic4-rating';
 
 const routes: Routes = [
   {
@@ -19,7 +20,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    IonicRatingModule
   ],
   declarations: [ProductoEditPage]
 })
