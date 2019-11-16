76f63fcce36cc89e8d8e2d0bcf395acf576837fa
Edward Fabian Baron <edwardbaron99@gmail.com>
2019-11-16 00:44:53 -0500

    Edit: AppComponent and AuthGuard

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,10 +1,6 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate, Router } from '@angular/router';
 import { UserServices } from 'src/services/UserServices';
-import { environment } from 'src/environments/environment';
-import { FirebaseApp } from '@angular/fire';
-import * as firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +9,15 @@ export class AuthGuard implements CanActivate {
   constructor(private router: Router, private uService: UserServices) {
   }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+  canActivate() {
     if (!localStorage.getItem('sesion')) {
       console.log('No has iniciado sesion.');
       return this.router.navigateByUrl('/login');
     }
     console.log('Acceso Exitoso');
-    if(!this.uService.user)
+    if (!this.uService.user) {
       this.uService.setUserJSON(localStorage.getItem('sesion'));
+    }
     return true;
   }
 }
