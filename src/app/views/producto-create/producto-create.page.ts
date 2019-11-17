3cc0a4fa2ff26500b318a88049e511cea77f5b22
Edward Fabian Baron <edwardbaron99@gmail.com>
2019-11-17 12:36:37 -0500

    Fix: Directorio del servicios y modelos en el ts

--- src/app/views/producto-create/producto-create.page.ts
+++ src/app/views/producto-create/producto-create.page.ts
@@ -4,9 +4,9 @@ import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/camera/n
 import { File } from '@ionic-native/file/ngx';
 import { WebView } from '@ionic-native/ionic-webview/ngx';
 import { Storage } from '@ionic/storage';
-import { product } from 'src/model/product';
-import { UserServices } from 'src/services/UserServices';
-import { ProductServices } from 'src/services/ProductServices';
+import { Product } from '../../models/product';
+import { UserServices } from '../../services/UserServices';
+import { ProductServices } from '../../services/ProductServices';
 
 const STORAGE_KEY = 'my_imgs';
 
@@ -31,26 +31,35 @@ export class ProductoCreatePage implements OnInit {
   @ViewChild('mySlider', null) slides: IonSlides;
 
   // tslint:disable-next-line: max-line-length
-  constructor(private alert: AlertController, private camera: Camera, private file: File, private storage: Storage, private plt: Platform, private webView: WebView, private actionSheetController: ActionSheetController, private toastController: ToastController,
+  constructor(
+    private alert: AlertController,
+    private camera: Camera,
+    private file: File,
+    private storage: Storage,
+    private plt: Platform,
+    private webView: WebView,
+    private actionSheetController: ActionSheetController,
+    private toastController: ToastController,
     private uService: UserServices,
     private pService: ProductServices) {
+
     this.ciudades = [
-      {name: 'Bogota',   value: 'A' },
-      {name: 'Cali',     value: 'B' },
-      {name: 'Medallo',  value: 'C' },
-      {name: 'El rosal', value: 'D' },
+      { name: 'Bogota', value: 'A' },
+      { name: 'Cali', value: 'B' },
+      { name: 'Medallo', value: 'C' },
+      { name: 'El rosal', value: 'D' },
     ];
 
     this.categorias = [
-      {name: 'Tecnologia',   value: 'A' },
-      {name: 'Celulares',     value: 'B' },
-      {name: 'Otros',  value: 'C' },
-      {name: 'Libros', value: 'D' },
-      {name: 'Electronica', value: 'E' },
-      {name: 'Laptops', value: 'F' },
-      {name: 'Ropa', value: 'G' },
-      {name: 'Tenis', value: 'H' },
-      {name: 'Calzado', value: 'I' }
+      { name: 'Tecnologia', value: 'A' },
+      { name: 'Celulares', value: 'B' },
+      { name: 'Otros', value: 'C' },
+      { name: 'Libros', value: 'D' },
+      { name: 'Electronica', value: 'E' },
+      { name: 'Laptops', value: 'F' },
+      { name: 'Ropa', value: 'G' },
+      { name: 'Tenis', value: 'H' },
+      { name: 'Calzado', value: 'I' }
     ];
     this.images = new Array<any>();
   }
@@ -127,7 +136,7 @@ export class ProductoCreatePage implements OnInit {
       const imag = this.webView.convertFileSrc(imageData);
       console.log(imageData);
       console.log(imag);
-      this.images.push({path: imag});
+      this.images.push({ path: imag });
       this.presentToast('Foto subida correctamente');
     }, (err) => {
       console.log('Flasho o cancelo');
@@ -157,7 +166,6 @@ export class ProductoCreatePage implements OnInit {
     this.presentAlert();
   }
 
-
   // Pruebas
   segmentChanged(ev: any) {
     console.log('Segmento cambiado', this.stateProd);
@@ -170,12 +178,12 @@ export class ProductoCreatePage implements OnInit {
   onRateChange(event) {
     console.log('Calificación:', event);
   }
-  createProduct(){
-    let p : product = new product();
+  createProduct() {
+    const p: Product = new Product();
     p.name = this.name;
     p.description = this.description;
     p.category = this.ctg;
-    p.state = this.stateProd != 0;
+    p.state = this.stateProd !== 0;
     p.user = this.uService.user.id;
     p.date = new Date();
     p.city = this.uService.user.city;
